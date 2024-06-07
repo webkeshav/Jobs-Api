2211499c4d40bbdf8b14ac69b456f9a1634b6a8a
Keshav <keshavtomar999@gmail.com>
2024-06-08 03:41:13 +0530

    Job routes

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -14,7 +14,7 @@ const auth = async (req,res,next)=>{
     try {
         const payload = jwt.verify(token,process.env.JWT_SECRET)
         //attach the user to the job routes
-        req.user = {user : payload.userId,name : payload.name}
+        req.user = {userId : payload.userId,name : payload.name}
         next()
     } catch (error) {
         throw new UnauthenticatedError('Authentication Invalid')
